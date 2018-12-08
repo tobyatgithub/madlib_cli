4646bff7b99706e5c603cedfb717f9298323e17f
tobyatgithub <toby.fangyuan@gmail.com>
2018-12-07 16:27:20 -0800

    all functions other than run() now have test

--- test_madlib.py
+++ test_madlib.py
@@ -1,4 +1,5 @@
-from madlib import run, extract_key_words, format_story
+import madlib
+from madlib import run, extract_key_words, format_story, welcome
 
 
 ln_one = "I the {Adjective} and {Adjective} {A First Name} have {Past Tense Verb} " \
@@ -10,6 +11,32 @@ ln_two = "What are a {Large Animal} and backpacking {Small Animal} to do? " \
 "worlds connected to A {First Name's} Lair. There are {Number} {Plural Noun} in"\
 "the game, along with hundreds of other goodies for you to find.\n\n"
 
+# impressive...use capsys to test print outs!
+def test_welcome(capsys):
+    madlib.welcome()
+    captured = capsys.readouterr()
+    assert captured.out == 'Welcome to the Jungle\n'
+
+# test read_file()
+def test_read_file():
+   content = madlib.read_madlib('./example_text.txt')[1]
+   assert content.startswith('Make Me A Video Game!')
+
+# test write_file()
+def test_output_finished():
+    content = 'texting content balabala'
+    path = './sample_output_finished.txt'
+    madlib.output_finished(content, path)
+    with open(path,'r') as f:
+        assert f.read() == content
+
+# test print_finished
+def test_print_finished(capsys):
+    madlib.print_finished("balaba")
+    captured = capsys.readouterr()
+    assert captured.out.startswith("\n\n...\nHere is the most creative paragraph in this universe!!!\n\n")
+
+
 # test for run()
 def test_run_exist():
     assert(run)
@@ -19,7 +46,7 @@ def test_extract_key_words():
     ln_one = "I the {Adjective} and {Adjective} {A First Name} have {Past Tense Verb} " \
     "{A First Name}'s {Adjective} sister and plan to steal her {Adjective} {Plural Noun}!\n"
 
-    actual = extract_key_words(ln_one)    
+    actual = extract_key_words(ln_one)
     expected1 = ['Adjective', 'Adjective', 'A First Name', 'Past Tense Verb', 'A First Name', 'Adjective', 'Adjective', 'Plural Noun']
     # notice that copy paste string from other source may cause error
     expected2 = "I the %s and %s %s have %s %s's %s sister and plan to steal her %s %s!\n"
@@ -36,13 +63,13 @@ def test_extract_key_words2():
     assert(expected2 == actual[1])
 
 
-# test for formating, basically it's one line with ("%s" %string)    
+# test for formating, basically it's one line with ("%s" %string)
 def test_format_story1():
     assert(format_story("111 %s %s %s", ["a","b","c"])=="111 a b c")
 
 def test_format_story1():
     assert(format_story("{} %s 231 %s fsaddd %s pumpkin %s\n", ["1","bob","cat", "500"])==\
-    "{} 1 231 bob fsaddd cat pumpkin 500\n")    
+    "{} 1 231 bob fsaddd cat pumpkin 500\n")
 
 
 # really not sure how to test other functions....
